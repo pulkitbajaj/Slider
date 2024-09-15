61b5c8c5fafaba0b5564ca324189eadf37cb060a
pulkitbajaj <83494078+pulkitbajaj@users.noreply.github.com>
2024-09-16 01:09:53 +0530

    Update SliderDemo.java

--- SliderDemo.java
+++ SliderDemo.java
@@ -1,14 +1,29 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.*;
-
+/*
+ SliderDemo is a Java Swing application that demonstrates the use of a JSlider component.
+The slider allows the user to select a value between 0 and 100, and the corresponding
+ temperature in degrees Celsius is displayed in a JLabel.
+ 
+ Example:
+  To run the application, create an instance of the SliderDemo class and call its
+  default constructor. This will create a JFrame with a JSlider and a JLabel that
+  displays the current temperature.
+*/
 
 
 public class SliderDemo implements ChangeListener{
 
+
+
     JFrame frame;
-    JPanel panel;
-    JLabel label;
+    JPanel panel; // panel is a container which can holds button, texts , labels etc.
+
+    // The JLabel that displays the current temperature.
+    JLabel label; //A Label object is a component for placing text in a container. A label displays a single line of read-only text. The text can be changed by the application, but a user cannot edit it directly.
+
+    // The JSlider(a component) that allows the user to select a value.
     JSlider slider;
 
     SliderDemo(){
@@ -22,6 +37,7 @@ public class SliderDemo implements ChangeListener{
         
         slider.setPaintTicks(true);
         slider.setMinorTickSpacing(10);
+        
         
         
         slider.setPaintTrack(true);
@@ -43,6 +59,8 @@ public class SliderDemo implements ChangeListener{
         panel.add(label);
         frame.add(panel);
 
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setVisible(true);
     }
